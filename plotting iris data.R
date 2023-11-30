c0948cfef69596399c6c918fb19f7e24297a45e0
Yuanhang Yang <yangdamien@gmail.com>
2023-11-30 18:41:13 +0100

    Save R code.

--- plotting iris data.R
+++ plotting iris data.R
@@ -14,3 +14,5 @@ ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) +
 ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) + 
   geom_point() +
   geom_smooth()
+
+renv::snapshot()
